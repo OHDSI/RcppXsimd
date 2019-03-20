c7483fd27e2b4cce67c144f5086478cb71a2b365
Marc Suchard <msuchard@ucla.edu>
2019-03-20 15:51:01 -0700

    initial commit

---
+++ inst/include/xsimd/xsimd.hpp
@@ -0,0 +1,21 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XSIMD_HPP
+#define XSIMD_HPP
+
+#include "memory/xsimd_alignment.hpp"
+#include "config/xsimd_config.hpp"
+#include "types/xsimd_traits.hpp"
+#include "math/xsimd_math.hpp"
+#include "math/xsimd_math_complex.hpp"
+#include "memory/xsimd_load_store.hpp"
+#include "stl/algorithms.hpp"
+
+#endif
+
---
+++ maintanence/download_xsimd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+VERSION=7.1.3
+FILE=${VERSION}.tar.gz
+URL=https://github.com/QuantStack/xsimd/archive/${FILE}
+VALID="MD5 (${FILE}) = 1d831e7f6ca87aa75b09c6e88911c18f"
+
+if [ ! -f "${FILE}" ]; then
+	wget ${URL}
+fi
+
+CHECK=`md5 ${VERSION}.tar.gz`
+
+echo "check: ${CHECK}"
+
+if [ "${CHECK}" != "${VALID}" ]; then
+	echo "Not valid download"
+	exit -1
+fi
+
+tar -xf ${FILE}
+
+mkdir -p ../inst
+cp -r xsimd-${VERSION}/include ../inst/.
+rm -rf xsimd-${VERSION}
